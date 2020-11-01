fc725549d4c7bc65e3d594936749c17a440c5893
Ben Thompson <benjamin.philip.thompson@maine.edu>
2020-11-01 00:17:46 -0400

    we can get a list of profssors from mainestreet

--- src/js/content_scripts/Mainestreet.js
+++ src/js/content_scripts/Mainestreet.js
@@ -4,21 +4,22 @@
 new MutationObserver(function(mutations) {
             for(let mutation of mutations) {
                 for(let node of mutation.addedNodes) {
-                        //if (!(node instanceof HTMLElement)) continue;	// we track only elements, skip other nodes (e.g. text nodes)
+                        //if (!(node instanceof HTMLElement)) continue;
                         
                         //for some reason mainestreet is inside an iframe so we have to do this to get the iframe content
                         //credit: https://stackoverflow.com/questions/1088544/get-element-from-within-an-iframe
                         var iframe = document.querySelector("#ptifrmtgtframe");
                         var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-                        if(innerDoc.querySelector("#DERIVED_SSS_SCL_TITLE1\\$78\\$")){
-                            console.log(innerDoc.querySelector("#DERIVED_SSS_SCL_TITLE1\\$78\\$").innerHTML);
-                        }
+                        if(innerDoc.querySelector("#MTG_INSTR\\$0")){ //if we found at least 1 professor find the rest :)
+                            var numOfProfessors = innerDoc.querySelectorAll("[id^=MTG_INSTR]");
+                            for(var i =0; i < numOfProfessors.length; i++){
+                                var currElement = numOfProfessors[i];
+                                console.log("Got prof name = " + currElement.innerHTML);
+                            }
+                            
 
-                        if(innerDoc.querySelector("#MTG_INSTR\\$0")){
-                            alert(innerDoc.querySelector("#MTG_INSTR\\$0").innerHTML);
                         }
-
                     }
                 }
         }).observe(document, {subtree: true, childList: true});
