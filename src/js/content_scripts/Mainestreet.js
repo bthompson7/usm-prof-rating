86b6bca445f0e3adbb96493f6bf6acfaabe446e9
Ben Thompson <benjamin.philip.thompson@maine.edu>
2020-11-01 14:44:59 -0500

    ratings work for mainestreet

--- src/js/content_scripts/Mainestreet.js
+++ src/js/content_scripts/Mainestreet.js
@@ -1,23 +1,34 @@
 //watch mainstreet for elements that appear
 //currently this gets the first professor in the course list
+var collegeName = "University+of+Southern+Maine"
 
 new MutationObserver(function(mutations) {
+
             for(let mutation of mutations) {
+
+                 
+               
+
                 for(let node of mutation.addedNodes) {
-                        //if (!(node instanceof HTMLElement)) continue;
-                        
                         //for some reason mainestreet is inside an iframe so we have to do this to get the iframe content
                         //credit: https://stackoverflow.com/questions/1088544/get-element-from-within-an-iframe
                         var iframe = document.querySelector("#ptifrmtgtframe");
                         var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-                        if(innerDoc.querySelector("#MTG_INSTR\\$0")){ //if we found at least 1 professor find the rest :)
+                        var titleElement = innerDoc.querySelector("#DERIVED_REGFRM1_UM_TITLE1")
+                        var dummyElement = innerDoc.querySelector("#RMP-DUMMY-ELEMENT")
+
+                        if(innerDoc.querySelector("#MTG_INSTR\\$0") && dummyElement == null){
                             var numOfProfessors = innerDoc.querySelectorAll("[id^=MTG_INSTR]");
                             for(var i =0; i < numOfProfessors.length; i++){
+                                var element = innerDoc.querySelectorAll("[class^=rmp-rating]");
                                 var currElement = numOfProfessors[i];
-                                console.log("Got prof name = " + currElement.innerHTML);
+                                var professorName = currElement.innerHTML;
+                                var splitName = professorName.split(" ");
+                                searchForProfessor(splitName[0],splitName[splitName.length - 1],collegeName,currElement); 
                             }
-                            
+
+                            titleElement.insertAdjacentHTML('afterend', '<p id="RMP-DUMMY-ELEMENT">..</p>');
 
                         }
                     }
--- src/js/util/util.js
+++ src/js/util/util.js
@@ -170,26 +170,26 @@ function jsonToHTML(jsonObject){
       var avgProfRating = jsonObject['avgRating'];
   
       if(avgProfRating >= 3.4){
-        return "<img src=" + chrome.extension.getURL('./assets/rmp-good.jpg') +
+        return "<img id='rmp-img' src=" + chrome.extension.getURL('./assets/rmp-good.jpg') +
          "><br><b>Overall Rating: </b>" + jsonObject['avgRating'] + "/5 based on " + 
          jsonObject['totalRatings'] + " ratings. <br><b>Difficulty: </b>" + jsonObject['profHardness'] +
           "/5<br>" + "<a href=" + ratingURL + jsonObject['profID'] + " target='_blank' >View Ratings on RateMyProfessors.com</a>";
   
       }else if(avgProfRating >= 2.5 && avgProfRating <= 3.3){
   
-        return "<img src=" + chrome.extension.getURL('./assets/rmp-average.jpg') +
+        return "<img id='rmp-img' src=" + chrome.extension.getURL('./assets/rmp-average.jpg') +
         "><br><b>Overall Rating: </b>" + jsonObject['avgRating'] + "/5 based on " + 
         jsonObject['totalRatings'] + " ratings. <br><b>Difficulty: </b>" + jsonObject['profHardness'] +
          "/5<br>" + "<a href=" + ratingURL + jsonObject['profID'] + " target='_blank'>View Ratings on RateMyProfessors.com</a>";
       }else if(avgProfRating <= 2.4){
   
-        return "<img src=" + chrome.extension.getURL('./assets/rmp-poor.jpg') +
+        return "<img id='rmp-img' src=" + chrome.extension.getURL('./assets/rmp-poor.jpg') +
         "><br><b>Overall Rating: </b>" + jsonObject['avgRating'] + "/5 based on " + 
         jsonObject['totalRatings'] + " ratings. <br><b>Difficulty: </b>" + jsonObject['profHardness'] +
          "/5<br>" + "<a href=" + ratingURL + jsonObject['profID'] + " target='_blank'>View Ratings on RateMyProfessors.com</a>";
       }
     }else{
-      return "<img src=" + chrome.extension.getURL('./assets/rmp-zero.jpg') + "><br><b>Overall Rating: </b>No ratings were found <br><b>Difficulty:</b> No ratings were found ";
+      return "<img id='rmp-img' src=" + chrome.extension.getURL('./assets/rmp-zero.jpg') + "><br><b>Overall Rating: </b>No ratings were found <br><b>Difficulty:</b> No ratings were found ";
     }
   
   }
